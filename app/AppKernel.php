c669dd6673a2aa698857e7176122bb4b8dd24705
louisemunier <munier.louise0@gmail.com>
2017-01-19 07:06:43 +0100

    Added directories materialize & generate TravelBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new ExamBlancBundle\ExamBlancBundle(),
+            new TravelBundle\TravelBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
